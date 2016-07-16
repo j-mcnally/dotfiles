dde153c882ff78dd1d05a9771b792ec6936366ee
Justin McNally <justin@kohactive.com>
2016-07-15 19:51:00 -0500

    update dotfiles

--- custom/env.zsh
+++ custom/env.zsh
@@ -1,4 +1,5 @@
 
 export PATH=/usr/local/opt/gnu-tar/libexec/gnubin:$PATH:/Development/android/build-tools/23.0.1:/Applications/LibreOffice.app/Contents/MacOS:/Development/android/tools:/Development/android/platform-tools
 export PATH=/usr/local/Cellar/postgresql/9.3.5_1/bin:$PATH
+export PATH=$PATH:/usr/local/m-cli
 export EDITOR=atom
